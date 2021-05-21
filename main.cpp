31be7c40aa9fec6b01db80e320faabf77bb3969b
MaryPetr <mary.petrosyants@yandex.ru>
2021-05-21 13:30:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,24 @@
 using namespace std;
 
 template <typename Iter>
-void w( Iter begin, Iter end)
+void print( Iter begin, Iter end)
 {
     set<int> mySet;
-	copy(begin, end, back_inserter(mySet));
-    
+	copy(begin, end, inserter(maset, maSet.end()));
+	copy(mySet.begin(), maSet.end(), ostream_iterator<string>(cout, ", "));
+	cout << endl;
 
 }
 
 int main() {
+	istream_iterator<string>beg_iter{ cin };
+	istream_iterator<string>end_iter;
+	print(beg_iter, end_iter);
 
+	vector<string> w{ "gdf","rgfhghghfg","gsdgsdghdshhd" };
+	print(w.begin(), w.end());
+
+	string w2 [3]{ "gdf","rgfhghghfg","gsdgsdghdshhd" };
+	print(begin(w2), end(w2));
 	return 0;
 }
